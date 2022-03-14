6ae133b0a476305f5aab8ab576056813e28d14d1
Nicolas Piñeros Campo <onpc1224@gmail.com>
2022-03-14 16:22:59 -0500

    Estructura proyecto

--- src/main/resources/static/js/apimock.js
+++ src/main/resources/static/js/apimock.js
@@ -4,7 +4,7 @@ apimock=(function(){
 
 	var mockdata=[];
 
-	mockdata["johnconnor"]=	[{author:"johnconnor","points":[{"x":150,"y":120},{"x":215,"y":115}],"name":"house"},
+	mockdata["johnconnor"]=	[{author:"johnconnor","points":[{"x":150,"y":150},{"x":150,"y":300},{"x":300,"y":300},{"x":300,"y":150}],"name":"house"},
 	 {author:"johnconnor","points":[{"x":340,"y":240},{"x":15,"y":215}],"name":"gear"}];
 	mockdata["maryweyland"]=[{author:"maryweyland","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"house2"},
 	 {author:"maryweyland","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"gear2"}];
@@ -28,7 +28,7 @@ apimock=(function(){
 				mockdata[authname].find(function(e){return e.name===bpname})
 			);
 		},
-		
+
 	}
 
 })();
